e2ce8264650592d893e93c8880fed9eb1b1c07a8
Alvin <alvinzcoxiv@gmail.com>
2020-06-19 23:28:35 -0400

    Timer working for quiz

--- script.js
+++ script.js
@@ -16,9 +16,8 @@ var submitScore = document.querySelector("#submitscore");
 var goBack = document.querySelector("#goback");
 var clearScores = document.querySelector("#clearscores");
 var timeLeft = document.querySelector("#timeleft");
-
 // Shows the user if the correct answer has been selected
-function correct() {
+function chkCorrect() {
     var showFor = 1
 
     var checkInterval = setInterval(function() {
@@ -33,7 +32,7 @@ function correct() {
 }
 
 // Show user if the wrong answer has been selected
-function wrong() {
+function chkWrong() {
     var showFor = 1
 
     var checkInterval = setInterval(function() {
@@ -48,63 +47,29 @@ function wrong() {
 }
 
 // Show time left for quiz (shows 0 seconds before start of quiz)
-timeLeft = 0;
-timeLeft.textContent = "Time left: " + timeLeft;
+var timer = 0;
+timeLeft.append("Time left: " + timer)
 
 // Function to run the quiz
 function runQuiz() {
 
 
     // Set time allowed for quiz to be taken
-    timeLeft = 75;
+    timer = 75;
+    
 
     // Begin countdown
     var timeInterval = setInterval(function() {
-        timeLeft.textContent = timeLeft;
-        timeLeft--;
+        timeLeft.textContent = "Time left: " + timer;
 
-        // A for loop to cycle through the questions
-        for (var i = 0; i < 7; i++) {
+        console.log(timeLeft);
+        timer--;
 
-            if (i = 0) {
-                // Hide introduction to quiz and show question 1
-                intro.setAttribute("style", "display:none;");
-                questOne.setAttribute("style", "display:block;");
-            }
-            else if (i = 1) {
-                // Hide question 1 and show question 2
-                questOne.setAttribute("style", "display:none;");
-                questTwo.setAttribute("style", "display:block;");
-            }
-            else if (i = 2) {
-                // Hide question 2 and show question 3
-                questTwo.setAttribute("style", "display:none;");
-                questThree.setAttribute("style", "display:block;");
-            }
-            else if (i = 3) {
-                // Hide question 3 and show question 4
-                questThree.setAttribute("style", "display:none;");
-                questFour.setAttribute("style", "display:block;");
-            }
-            else if (i = 4) {
-                // Hide question 4 and show question 5
-                questFour.setAttribute("style", "display:none;");
-                questFive.setAttribute("style", "display:block;");
-            }
-            else if (i = 5) {
-                // Hide question 5 and show All Done
-                questFive.setAttribute("style", "display:none;");
-                allDone.setAttribute("style", "display:block;");
-                clearInterval(timeInterval);
-            }
-            else {
-                // Hide All Done and show View High Scores
-                allDone.setAttribute("style", "display:none;");
-                viewScores.setAttribute("style", "display:block;");
-            }
+        if (timer === 0) {
 
-            
+            clearInterval(timeInterval);
         }
-
-    })
+    }, 1000)
 }
+
+startQuiz.addEventListener("click", runQuiz);
